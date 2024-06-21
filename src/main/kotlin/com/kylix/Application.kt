0ff34e4ed87d06d5813cd86963ba17b4af3857fc
Denys Denysenko <farenotwells@gmail.com>
2024-06-21 10:25:42 +0300

    fixed get2

--- src/main/kotlin/com/kylix/Application.kt
+++ src/main/kotlin/com/kylix/Application.kt
@@ -7,18 +7,18 @@ import com.kylix.plugins.*
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "localhost", module = Application::module)
-        .start(wait = true)
-}
+//fun main() {
+//    embeddedServer(Netty, port = 8080, host = "localhost", module = Application::module)
+//        .start(wait = true)
+//}
 
 val userApplications = mutableMapOf<Long, UserApplication>()
 val userApplicationRepository = UserApplicationRepository("mongodb+srv://testuser:Wrs0DTzoA2V4LGgp@cluster0.m7q3j0q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-//fun main() {
-//    val port = System.getenv("PORT")?.toInt() ?: 8080
-//    embeddedServer(Netty, port = port, host = "0.0.0.0", module = Application::module)
-//        .start(wait = true)
-//}
+fun main() {
+    val port = System.getenv("PORT")?.toInt() ?: 8080
+    embeddedServer(Netty, port = port, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
 fun Application.module() {
     FirebaseAdmin.init()
     configureRouting()
